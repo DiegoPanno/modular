3438f8ea6d3d104fcf355c90fac512c2f88ed642
DiegoPanno <diegompanno@gmail.com>
2026-02-04 15:23:48 -0300

    ajuste form

--- script.js
+++ script.js
@@ -12,19 +12,22 @@ function alertarInteres(modelo) {
 }
 
 document.getElementById('contactForm').addEventListener('submit', function(e) {
-    e.preventDefault();
-    
-    const myForm = e.target;
-    const formData = new FormData(myForm);
-    
-    fetch("/", {
-        method: "POST",
-        headers: { "Content-Type": "application/x-www-form-urlencoded" },
-        body: new URLSearchParams(formData).toString(),
-    })
-    .then(() => {
-        alert('¡Gracias! Hemos recibido tu consulta y te responderemos al mail indicado.');
-        myForm.reset();
-    })
-    .catch((error) => alert('Error: ' + error));
+  e.preventDefault();
+  
+  const myForm = e.target;
+  const formData = new FormData(myForm);
+  
+  // Aseguramos que el nombre coincida con el nuevo name del form
+  formData.append('form-name', 'nexus-form');
+
+  fetch("/", {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: new URLSearchParams(formData).toString(),
+  })
+  .then(() => {
+    alert('¡Recibido! Nos comunicaremos con vos a la brevedad.');
+    myForm.reset();
+  })
+  .catch((error) => alert('Error de conexión: ' + error));
 });
