86f143bf5307e6de8dce71301aa876186ac9c8f7
DiegoPanno <diegompanno@gmail.com>
2026-02-04 15:54:05 -0300

    form

--- script.js
+++ script.js
@@ -18,17 +18,20 @@ document.getElementById("contactForm").addEventListener("submit", function (e) {
 
   const myForm = e.target;
   const formData = new FormData(myForm);
-  // Esto le dice a Netlify: "Guardame esto en el cajón de contacto-nexus"
-  formData.append("form-name", "contacto-nexus");
 
+  // Verificamos que se envíe el nombre correcto del formulario
   fetch("/", {
     method: "POST",
     headers: { "Content-Type": "application/x-www-form-urlencoded" },
     body: new URLSearchParams(formData).toString(),
   })
-    .then(() => {
-      alert("¡Recibido! Nos comunicaremos con vos a la brevedad.");
-      myForm.reset();
+    .then((response) => {
+      if (response.ok) {
+        alert("¡Recibido! Nos comunicaremos con vos a la brevedad.");
+        myForm.reset();
+      } else {
+        throw new Error("Respuesta del servidor no válida");
+      }
     })
     .catch((error) => alert("Error de conexión: " + error));
 });
